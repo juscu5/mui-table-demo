1d4bee5086af509ac05aa83b2b7a0fb151d981a9
juscu5 <cunananjunel@gmail.com>
2024-12-18 20:54:26 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./index.css";
+import DataTable from "./components/DataTable";
+//import SimpleTable from "./components/SimpleTable";
+
+function App() {
+  return (
+    <div style={{ padding: 10 }}>
+      <DataTable />
+      {/* <SimpleTable /> */}
+      {/* <DataTable /> */}
+    </div>
+  );
+}
+
+export default App;
